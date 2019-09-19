cc5340c8391849e82137032aebacbc1e2307617a
Ross Lorimer <wross.lorimer@googlemail.com>
2019-09-19 14:21:50 +0100

    10 tests complete

--- star_system.rb
+++ star_system.rb
@@ -22,4 +22,27 @@ class StarSystem
     @planets.min { |planet1, planet2| planet1.diameter <=> planet2.diameter}
   end
 
+  def get_planets_with_no_moons
+    @planets.find_all { |planet| planet.number_of_moons == 0}
+  end
+
+  def get_planets_with_more_moons(number)
+    moony_planets = []
+    @planets.find_all do |planet|
+      if planet.number_of_moons > number
+        moony_planets.push(planet.name)
+      end
+    end
+    return moony_planets
+  end
+
+  def get_number_of_planets_closer_than(distance)
+    @planets.find_all { |planet| planet.distance_from_sun < distance}.length
+  end
+
+  def get_total_number_of_moons
+    @planets.reduce(0) { |total, planet| total + planet.number_of_moons}
+  end 
+
+
 end
